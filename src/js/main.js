9f68291127e7f72a7e07e141969d52b76ddaf783
StanTheMan7 <sts.graur@gmail.com>
2021-06-18 01:20:53 +0200

    rejout de tout mon style css rajout de nightmode et formulaire

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,39 @@
 let conectionButton = document.querySelector('.conection-formulaire');
 let tabblur = Array.from(document.getElementsByClassName('blur'));
-let addTodo = (event) => {
-    
-    // create div bg wight for the fomulaire 
-    let divContainer = document.body.appendChild(document.createElement('div'));
-    divContainer.classList.add('noblur');
-    
-    let buttonExit = document.divContainer.appendChild(document.createElement('button'));
-    buttonExit.classList.add('buttonExit');
-    buttonExit.innerText = 'X'
-
-}
-conectionButton.addEventListener('click', () => {
-    tabblur.forEach(element => {
-        element.style.filter ='blur(4px)';
-    });
-    addTodo();
+let formulaire1 = document.getElementsByClassName('conexion')[0];
+let formulaire2 = document.getElementsByClassName('conexion2')[0]
+let btnInscription = document.getElementsByClassName('conexionInscription')[0].lastElementChild
+let btnConection = document.getElementsByClassName('conexionInscription')[1].firstElementChild
+let btnExit =  document.getElementsByClassName('conexion')[0].firstElementChild.lastElementChild
+btnExit.addEventListener('click', () => {
+    formulaire1.style.display ='none'
+    formulaire2.style.display ='none'
 })
+console.log(btnExit);
+
+btnConection.addEventListener('click', ()=>{
+    formulaire1.classList.toggle('noblur')
+    formulaire1.style.display ='flex'
+    formulaire2.classList.toggle('noblur')
+    formulaire2.style.display ='none'
+    
+
+    
+    
+})
+
+conectionButton.addEventListener('click', () => {
+    
+    formulaire1.classList.toggle('noblur')
+    formulaire1.style.display ='flex'
+    formulaire2.classList.toggle('noblur')
+    formulaire2.style.display ='none'
+        
+    })
+    btnInscription.addEventListener('click', () =>{
+        
+        formulaire2.classList.toggle('noblur')
+        formulaire2.style.display ="flex";
+        formulaire1.classList.toggle('noblur')
+        formulaire1.style.display ='none'
+    })
